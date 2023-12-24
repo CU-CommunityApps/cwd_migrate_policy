6c273c1aa696dc49494966fc12ae9a7551db8ebf
Alison Jo <2660348+alisonjo315@users.noreply.github.com>
2023-12-24 00:48:48 -0500

    code comments

--- settings.migrate-on-pantheon.php
+++ settings.migrate-on-pantheon.php
@@ -1,5 +1,14 @@
 <?php
+
+// PUT THIS FILE IN web/sites/default/
+// ADD "include" TO web/sites/default/settings.php (refer to pantheon docs link below, and cwd_policy_fcs/README.md)
+
 /**
+/**
+ * @file
+ * Configuration file with source Drupal 7 site database credentials.
+ * DB creds can be updated with pantheon-systems/terminus-secrets-plugin, on
+ * whichever env you're migrating **INTO.**
  * Reference: https://pantheon.io/blog/running-drupal-8-data-migrations-pantheon-through-drush
  * Also: https://github.com/CU-CommunityApps/cwd_migrate_fcs
  */
